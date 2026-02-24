9d2b27eb258c92eca9730adc49a5806068ad6561
Mahfuz Saim <mahfuzsaim9@gmail.com>
2026-02-24 17:15:14 +0600

    Restaurant CRUD added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,16 @@ import express from 'express';
 
 const router = express.Router();
 import { authRoutes } from '../modules/auth/auth.routes';
+import { restaurantRoutes } from '../modules/restaurant/restaurant.routes';
 
 const allRoutes = [
     {
         path: '/auth',
         route: authRoutes,
+    },
+    {
+        path: '/restaurant',
+        route: restaurantRoutes,
     },
 ];
 

1e2479e655cab0c8bbf055d33cf6c9a986f4e797
Naim <naim.baijid@gmail.com>
2026-02-25 18:57:50 +0600

    fix msg sending

--- src/sockets/waiterSocket.ts
+++ src/sockets/waiterSocket.ts
@@ -15,20 +15,22 @@ export const setupWaiterSocketNamespace = (io: Server) => {
     // waiterSocket.use(verifyJwt(UserRole.WAITER));
     //
     waiterSocket.on('connection', (socket: Socket) => {
+        const room = '123';
         console.log(
             'Waiter Socket connected:',
             socket.id,
             'user:',
             socket.user?.id,
         );
-        socket.join('123'); // Join a room based on the user ID for targeted messaging
+        socket.join(room); // Join a room based on the user ID for targeted messaging
         socket.emit('connected', {
             message: 'Welcome to the waiter socket namespace!',
         });
         //test
-        socket.on('test', data => {
+        socket.on('test', async data => {
             console.log('Received test event with data:', data);
-            socket.in('123').emit('testResponse', {
+            const roomMembers = await waiterSocket.in(room).fetchSockets();
+            waiterSocket.to(room).emit('testResponse', {
                 message: 'Test event received successfully!',
                 receivedData: data,
             });

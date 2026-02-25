6c01c18808b1b75edd9a7340ea6f8315cb483bfa
Naim <naim.baijid@gmail.com>
2026-02-25 16:25:50 +0600

    add socket middleware

---
+++ src/middlewares/socket.middleware.ts
@@ -0,0 +1,68 @@
+import httpstatus from 'http-status';
+import prisma from '../utils/prisma';
+import { Socket } from '../types/socket.types';
+import { AuthUtils } from '../utils/AuthUtils';
+
+export const verifyJwt =
+    (...requiredRoles: string[]) =>
+    async (socket: Socket, next: any) => {
+        try {
+            const accessToken = socket.handshake.auth?.token;
+            if (!accessToken) {
+                const err = new Error('Token missing') as any;
+                err.data = {
+                    status: httpstatus.UNAUTHORIZED,
+                    message: 'No auth token provided',
+                };
+                return next(err);
+            }
+            const verifiedToken = AuthUtils.verifyAccessToken(accessToken);
+
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: verifiedToken?.id,
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    fullName: true,
+                    isVerified: true,
+                    role: true,
+                },
+            });
+            if (!user) {
+                const err = new Error('Invalid Token') as any;
+                err.data = {
+                    status: httpstatus.UNAUTHORIZED,
+                    message: 'Invalid token',
+                };
+                return next(err);
+            }
+            if (!user.isVerified) {
+                const err = new Error('Unverified Email') as any;
+                err.data = {
+                    status: httpstatus.UNAUTHORIZED,
+                    message: 'Please verify your email',
+                };
+                return next(err);
+            }
+            socket.user = user;
+
+            if (requiredRoles.length && !requiredRoles.includes(user.role)) {
+                const err = new Error('Forbidded') as any;
+                err.data = {
+                    status: httpstatus.FORBIDDEN,
+                    message: 'Forbidded',
+                };
+                return next(err);
+            }
+            next();
+        } catch (error) {
+            const err = new Error('Invalid token') as any;
+            err.data = {
+                status: httpstatus.UNAUTHORIZED,
+                message: 'Invalid token',
+            };
+            return next(err);
+        }
+    };

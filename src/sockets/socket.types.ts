89c856dfd7cebdb30f45200480ada05af09245ef
Naim <naim.baijid@gmail.com>
2026-02-26 13:02:52 +0600

    update socket type to include user and data

--- src/sockets/socket.types.ts
+++ src/sockets/socket.types.ts
@@ -1,5 +1,6 @@
-import { Socket as BaseSocket } from 'socket.io';
 import { User } from '@prisma/client';
+import type { Socket as IoSocket } from 'socket.io';
+import 'socket.io';
 
 // Only include the fields needed in the socket
 export type SocketUser = Pick<
@@ -7,6 +8,14 @@ export type SocketUser = Pick<
     'id' | 'email' | 'fullName' | 'isVerified' | 'role'
 >;
 
-export interface Socket extends BaseSocket {
-    user?: SocketUser;
+declare module 'socket.io' {
+    interface Socket {
+        user: SocketUser;
+    }
+
+    interface SocketData {
+        restaurantId?: string;
+    }
 }
+
+export type Socket = IoSocket;
